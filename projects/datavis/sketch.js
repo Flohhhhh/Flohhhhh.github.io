1350b7811bdbdec2006819a6d410b49b8f9444ed
Flohhhhh <camgus322@gmail.com>
2020-04-30 10:03:16 -0400

    Fixes

--- projects/datavis/sketch.js
+++ projects/datavis/sketch.js
@@ -1,4 +1,4 @@
-let table 
+let table
 let numRows
 let lng
 let lat
@@ -18,45 +18,46 @@ function setup() {
   image(bgimg,0,0,width,height);
   image(titleimg,0,0,991/2,225/2);
   image(rangeimg,0,height-167/2,908/2,167/2);
-  
+  createP('<p class=link><a href="https://www.kaggle.com/carlosparadis/fires-from-space-australia-and-new-zeland">Data Source: https://www.kaggle.com/carlosparadis/fires-from-space-australia-and-new-zeland</a></p> -->');
   noLoop();
-  
+
   blendMode(ADD);
-  
+
   numRows = table.getRowCount();
   print(numRows);
-  
+
   lat = table.getColumn('latitude');
   lng = table.getColumn('longitude');
   bright = table.getColumn('brightness');
-  
+
   minLat = min(lat);
   maxLat = max(lat);
-  
+
   minLng = min(lng);
   maxLng = max(lng);
-  
+
   minBright = min(bright);
-  maxBright = max(bright); 
-  
+  maxBright = max(bright);
+
   print (minLat, maxLat);
   print (minLng,maxLng);
   print (minBright, maxBright);
-  
+
 }
 
 function draw() {
-  
+
+
   noStroke();
 
      for (let i = 0; i < numRows; i++) {
-       
+
      let longMap = map(lng[i], minLng, maxLng, 50, width-50);
      let latMap = map(lat[i], minLat, maxLat, height-50, 50);
      let brightMap = map(bright[i], minBright, maxBright,2,20);
 
      image(img,longMap, latMap, brightMap, brightMap);
-     
+
    }
 
 }
--- projects/p5clock/sketch.js
+++ projects/p5clock/sketch.js
@@ -1,21 +1,21 @@
 function setup() {
   createCanvas(800, 800);
-
+  
   angleMode(DEGREES)
   imageMode(CENTER)
-
+  
   bg = loadImage('https://i.imgur.com/jbtIgxm.png');
   mnthimg = loadImage('https://i.imgur.com/iAesZeO.png');
   dimg = loadImage('https://i.imgur.com/MNi1OFY.png');
   hrimg = loadImage('https://i.imgur.com/BMJadX7.png');
   miimg = loadImage('https://i.imgur.com/TpJ6mwm.png');
   secimg = loadImage('https://i.imgur.com/dSdQqKn.png');
-
+  
   frameRate(2)
 }
 
 function draw() {
-
+  
   //get time
   let yr = year();
   let mth = month();
@@ -23,20 +23,20 @@ function draw() {
   let hr = hour();
   let min = minute();
   let sec = second();
-
+  
   let mthend = map(mth, 1,12,0,360)
   let dend = map(d, 1,30,0,360)
   let hrend = map(hr % 12, 1,12,0,360)
   let minend = map(min, 0,60,0,360)
   let secend =map(sec, 0,60,0,360)
-
-
+  
+  
   translate(400,400)
   push()
   background(220);
-
+  
   image(bg, 0, 0);
-
+  
   //MONTH
   rotate(mthend)
 
